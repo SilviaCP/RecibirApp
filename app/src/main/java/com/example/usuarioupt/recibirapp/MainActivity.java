413049ae6255dbb41b150aa94d1e80a7c9cdea90
USUARIOUPT <USUARIOUPT>
2017-03-25 11:23:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/usuarioupt/recibirapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.usuarioupt.recibirapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if(Intent.ACTION_SEND.equals(action) && type != null){
+            if("text/plain".equals(type)){
+                //manipulamos la cadena enviada
+                manipularTexto(intent);
+            }
+        }
+    }
+
+    public void manipularTexto(Intent intent){
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if(sharedText != null){
+            Toast.makeText(MainActivity.this,sharedText,Toast.LENGTH_LONG).show();
+        }
+    }
+}
